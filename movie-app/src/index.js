28ee4a213e3034ebded2b3684d5a510ce29873f5
Ambika Kumari <kambika1309@gmail.com>
2020-07-30 01:32:22 +0530

    Added middleware

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import './index.css';
 import App from './components/App';
 import movies from './reducers/index';
+
+
+//function logger (obk,next,action)
+//logger(obj)(next)(action)
+const logger=function({dispatch,getState}){
+  return function(next){
+
+    return function(action){
+      //middleware code
+      console.log('ACTION_TYPE =',action.type);
+      next(action);
+    }
+  }
+
+}
 //store requires movie argument as reducer
-const store =createStore(movies);
+const store =createStore(rootReducer,applyMiddleware(logger));
 // console.log('store',store);
 // console.log('STATE',store.getState());
 // store.dispatch({
